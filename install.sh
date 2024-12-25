657be9dda2005b12a48ab8a722fe817d8c44b28b
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-25 13:36:57 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -10,37 +10,37 @@ mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2
 
 #Update Packages
-echo 'Updating default packages\n' | lolcat
+echo "Updating default packages" | lolcat
 pkg update && pkg upgrade -y
 sleep 3
 
 #Install Aria2 Termux
-echo 'Installing Required Packages' | lolcat
+echo "Installing Required Packages" | lolcat
 pkg install aria2 -y
 sleep 2
 
 #Request Storage Access
-echo 'Requesting access to storage' | lolcat
-echo 'Allow Storage Permission!' | lolcat
+echo "Requesting access to storage" | lolcat
+echo "Allow Storage Permission!" | lolcat
 termux-setup-storage
 sleep 2
 
 #Making Directories
-echo 'Making your new directory to download videos' | lolcat
-echo 'Creating bin folder' | lolcat
+echo "Making your new directory to download videos" | lolcat
+echo "Creating bin folder" | lolcat
 mkdir ~/storage/shared/Fam-Hacker_Client
 mkdir ~/bin
 mkdir ~/storage/shared/sfx
 sleep 3
 
 #Creating Termux-File-Editor Script
-echo 'Creating termux-file-editor Script' | lolcat
+echo "Creating termux-file-editor Script" | lolcat
 chmod +x termux-file-editor
 mv termux-file-editor ~/bin/
 sleep 2
 
 #Moving Sound Effects
-echo 'Moving Sound Effects to Fam-Hacker_Client folder.' | lolcat
+echo "Moving Sound Effects to Fam-Hacker_Client folder." | lolcat
 mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
 mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
 mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
@@ -56,6 +56,6 @@ toilet -f big 'Milked' | lolcat
 echo " "
 echo " "
 echo " "
-echo 'Now you can open any .torrent file and it will open in termux start Downloading.' | lolcat
+echo "Now you can open any .torrent file and it will open in termux start Downloading." | lolcat
 sleep 5
 clear
