ea4cd44f265f0d98e00742e2066276d4df66b1b4
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 13:30:06 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,44 +5,44 @@
 # Date : 12/04/2024
 
 clear
-'date' | lolcat -f --force -seed=9000 --animate
+'date' | lolcat -f --force --seed=9000
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2
 
 #Update Packages
-echo -e "\e[038m" "Updating default packages\n"
+echo -e "\e[39m" "Updating default packages\n"
 pkg update && pkg upgrade -y
 sleep 3
 
 #Install Aria2 Termux
-echo -e "\e[039m" "Installing Required Packages\n"
+echo -e "\e[38m" "Installing Required Packages\n"
 pkg install aria2 -y
 sleep 2
 
 #Request Storage Access
-echo -e "\e[032m" "Requesting access to storage\n"
+echo -e "\e[37m" "Requesting access to storage\n"
 sleep 2
-echo -e "\e[031m" "Allow Storage Permission!\n"
+echo -e "\e[36m" "Allow Storage Permission!\n"
 sleep 2
 termux-setup-storage
 sleep 5
 
 #Making Directories
-echo -e "\e[037m" "Making your new directory to download videos\n"
-echo -e "\e[038m" "Creating bin folder\n"
+echo -e "\e[35m" "Making your new directory to download videos\n"
+echo -e "\e[34m" "Creating bin folder\n"
 mkdir ~/storage/shared/Fam-Hacker_Client
 mkdir ~/bin
 mkdir ~/storage/shared/sfx
 sleep 3
 
 #Creating Termux-File-Editor Script
-echo -e "\e[038m" "Creating termux-file-editor Script\n"
-echo -e "\e[039m"  "chmod +x termux-file-editor\n"
+echo -e "\e[33m" "Creating termux-file-editor Script\n"
+echo -e "\e[32m"  "chmod +x termux-file-editor\n"
 mv termux-file-editor ~/bin/
 sleep 2
 
 #Moving Sound Effects
-echo -e "\e[039m" "Moving Sound Effects to Fam-Hacker_Client folder.\n" | lolcat
+echo -e "\e[031m" "Moving Sound Effects to Fam-Hacker_Client folder.\n" | lolcat
 mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
 mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
 mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
