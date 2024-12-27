264d10791be38caf97197e31450f2b3fcd71e942
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 13:44:18 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -16,15 +16,15 @@ sleep 3
 
 #Install Aria2 Termux
 echo -e "\e[31m" "Installing Required Packages\n"
-pkg install aria2 -y
+pkg install aria2 -y | lolcat
 sleep 2
 
 #Request Storage Access
-echo -e "\e[37m" "Requesting access to storage\n"
+echo -e "\e[33m" "Requesting access to storage\n"
 sleep 2
-echo -e "\e[36m" "Allow Storage Permission!\n"
+echo -e "\e[33m" "Allow Storage Permission!\n"
 sleep 2
-termux-setup-storage
+termux-setup-storage -y | lolcat
 sleep 5
 
 #Making Directories
@@ -36,18 +36,18 @@ mkdir ~/storage/shared/sfx
 sleep 3
 
 #Creating Termux-File-Editor Script
-echo -e "\e[31m" "Creating termux-file-editor Script\n"
-echo -e "\e[32m"  "chmod +x termux-file-editor\n" | lolcat -f --force
-mv termux-file-editor ~/bin/
+echo -e "\e[33m" "Creating termux-file-editor Script\n"
+echo -e "\e[33m" "chmod +x termux-file-editor\n"
+mv termux-file-editor ~/bin/ | lolcat -f --force
 sleep 2
 
 #Moving Sound Effects
-echo -e "\e[031m" "Moving Sound Effects to Fam-Hacker_Client folder.\n" | lolcat
-mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
-mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
-mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
-mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
-mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
+echo -e "\e[031m" "Moving Sound Effects to Fam-Hacker_Client folder.\n"
+mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3 | lolcat -f --force
+mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3 | lolcat -f --force
+mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/ | lolcat -f --force
+mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3 | lolcat -f --force
+mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3 | lolcat -f --force
 echo " "
 echo " "
 clear
@@ -58,6 +58,6 @@ toilet -f big 'Milked' | lolcat
 echo " "
 echo " "
 echo " "
-echo "Now you can open any .torrent file and it will open in termux start Downloading." | lolcat
+echo -e "\e[031m" "Now you can open any .torrent file and it will open in termux start Downloading.\n"
 sleep 5
 clear
