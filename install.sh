b046ee0855fd8d792c320b2906a909d52603d254
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 15:32:35 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,34 +5,43 @@
 # Date : 12/04/2024
 
 clear
-'date' | lolcat -f --force --seed=9000
+'date' | lolcat -f --force --seed=9000 --invert
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2
 
 #Update Packages
-echo "\033[32mUpdating\033[0m \033[38mdefault\033[0m \033[32mpackages\033[0m"
+echo ""
+echo -e "\e[1;38m" "Updating default packages\n"
+echo""
 pkg update && pkg upgrade -y
 sleep 3
 
 #Install Aria2 Termux
 echo "Installing Required Packages"
+echo ""
 pkg install aria2 -y
+echo ""
 sleep 2
 
 #Request Storage Access
-echo "Requesting access to storage"
+echo -e "\e[1;33m" "Requesting access to storage\n"
 sleep 2
+
 echo "Allow Storage Permission!"
 sleep 2
+echo " "
 termux-setup-storage
+echo " "
 sleep 5
 
 #Making Directories
 echo "Making your new directory to download videos"
 echo "Creating bin folder"
+echo " "
 mkdir ~/storage/shared/Fam-Hacker_Client
 mkdir ~/bin
 mkdir ~/storage/shared/sfx
+echo " "
 sleep 3
 
 #Creating Termux-File-Editor Script
@@ -44,8 +53,10 @@ sleep 2
 #Moving Sound Effects
 echo "Moving Sound Effects to Fam-Hacker_Client folder."  | lolcat -f --force
 mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
+mv ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
 mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
 mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
+mv ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
 mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
 mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
 echo " "
@@ -56,7 +67,8 @@ cowsay -f milk 'Process Complete' | lolcat
 toilet -f big 'Milked' | lolcat
 echo " "
 echo " "
-echo " "
+echo -e "\033[37m"
 echo "Now you can open any .torrent file and it will open in termux start Downloading."
+echo -e "\033[0m"
 sleep 15
 clear
