ae40dda91352f79eb6961d73867cc13bfe8680b2
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 14:05:39 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -10,44 +10,44 @@ mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2
 
 #Update Packages
-echo -e "\e[31m" "Updating default packages\n"
-pkg update && pkg upgrade -y | lolcat
+echo -e "\e[32m" "Updating default packages\n"
+pkg update && pkg upgrade -y >/dev/null 2>/dev/null
 sleep 3
 
 #Install Aria2 Termux
-echo -e "\e[31m" "Installing Required Packages\n"
-pkg install aria2 -y | lolcat
+echo -e "\e[32m" "Installing Required Packages\n"
+pkg install aria2 -y >/dev/null 2>/dev/null
 sleep 2
 
 #Request Storage Access
-echo -e "\e[31m" "Requesting access to storage\n"
+echo -e "\e[32m" "Requesting access to storage\n"
 sleep 2
-echo -e "\e[31m" "Allow Storage Permission!\n"
+echo -e "\e[32m" "Allow Storage Permission!\n"
 sleep 2
-termux-setup-storage -y | lolcat
+termux-setup-storage -y
 sleep 5
 
 #Making Directories
-echo -e "\e[31m" "Making your new directory to download videos\n"
-echo -e "\e[31m" "Creating bin folder\n"
+echo -e "\e[32m" "Making your new directory to download videos\n"
+echo -e "\e[32m" "Creating bin folder\n"
 mkdir ~/storage/shared/Fam-Hacker_Client
 mkdir ~/bin
 mkdir ~/storage/shared/sfx
 sleep 3
 
 #Creating Termux-File-Editor Script
-echo -e "\e[31m" "Creating termux-file-editor Script\n"
-chmod +x termux-file-editor | lolcat --seed=5500
-mv termux-file-editor ~/bin/ | lolcat -f --force
+echo "Creating termux-file-editor Script" | lolcat
+chmod +x termux-file-editor >/dev/null 2>/dev/null
+mv termux-file-editor ~/bin/ >/dev/null 2>/dev/null
 sleep 2
 
 #Moving Sound Effects
 echo -e "\e[031m" "Moving Sound Effects to Fam-Hacker_Client folder.\n"
-mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3 | lolcat -f --force
-mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3 | lolcat -f --force
-mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/ | lolcat -f --force
-mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3 | lolcat -f --force
-mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3 | lolcat -f --force
+mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
+mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
+mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
+mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
+mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
 echo " "
 echo " "
 
