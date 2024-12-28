d3ea94bfb6cf153ac185580410114565b6da62aa
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 20:16:39 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -31,12 +31,12 @@ echo -e '\033[1;33m'
 echo -e '\033[0m'
 sleep 5
 #Making Directories
-echo -e "\033[1;34mMaking your new directory to\033[0m \033[37mdownload\033[0m \033[1;34mvideos\033[0m"
+echo -e "\033[34mMaking your new directory to\033[0m \033[36mdownload\033[0m \033[34mvideos\033[0m"
 echo -e "\033[1;34m" "Creating bin folder\n"
 echo -e '\033[36m'
-'mkdir' ~/storage/shared/Fam-Hacker_Client
-'mkdir' ~/bin
-'mkdir' ~/storage/shared/sfx
+mkdir ~/storage/shared/Fam-Hacker_Client
+mkdir ~/bin
+mkdir ~/storage/shared/sfx
 echo -e '\033[0m'
 sleep 3
 #Creating Termux\033[34m-\033[0mFile-Editor Script
@@ -50,13 +50,13 @@ sleep 4
 #Moving Sound Effects
 echo -e "\033[1;34mMoving Sound Effects to Fam-Hacker_Client folder.\033[0m"
 echo -e '\033[1;36m'
-'mv' ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
-'mv' ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
-'mv' ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
-'mv' ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
-'mv' ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
-'mv' ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
-'mv' ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
+mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
+mv ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
+mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
+mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
+mv ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
+mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
+mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
 echo -e '\033[0m'
 echo " "
 sleep 7
@@ -68,5 +68,5 @@ echo " "
 echo " "
 echo -e "\033[34mNow you can open any .torrent file and it will open in termux start Downloading.\033[0m"
 echo ""
-sleep 15
+sleep 30
 clear
