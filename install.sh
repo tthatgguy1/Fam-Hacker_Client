ad0fbe3f3b48e4f4c866df214955acf447679752
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 19:44:50 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -11,53 +11,54 @@ sleep 2
 
 #Update Packages
 echo ""
-echo -e "\033[30mUpdating default packages\033[0m"
-echo -e "\033[1;34m"
-pkg update && pkg upgrade -y
+echo -e "\033[1;34mUpdating default packages\033[0m"
+echo -e '\033[1;34m'
+'pkg' update && 'pkg' upgrade -y
+echo -e '.\033[0m'
 sleep 3
-
 #Install Aria2 Termux
 echo -e "\033[36mInstalling Required Packages\033[0m"
-echo -e "\033[36m"
-pkg install aria2 -y
-echo -e " "
+echo -e '\033[1;36m'
+'pkg' install aria2 -y
+echo -e '.\033[0m'
 sleep 3
 
 #Request Storage Access
 echo -e "\033[33mRequesting access to storage\033[0m"
 echo -e "\033[1;31mAllow Storage Permission!\033[0m"
 echo -e "\033[1;31mAllow Storage Permission!\033[0m"
-echo -e "\033[1;33m"
-termux-setup-storage
-echo " "
+echo -e '\033[1;33m'
+'termux-setup-storage'
+echo -e '.\033[0m'
 sleep 5
 #Making Directories
 echo -e "\033[1;34mMaking your new directory to download videos\033[0m"
 echo -e "\033[1;34m" "Creating bin folder\n"
-echo -e "\033[36m"
-mkdir ~/storage/shared/Fam-Hacker_Client
-mkdir ~/bin
-mkdir ~/storage/shared/sfx
-echo " "
+echo -e '\033[36m'
+'mkdir' ~/storage/shared/Fam-Hacker_Client
+'mkdir' ~/bin
+'mkdir' ~/storage/shared/sfx
+echo -e '.\033[0m'
 sleep 3
 #Creating Termux\033[34m-\033[0mFile-Editor Script
 echo -e "Creating termux\033[33m-\033[0mfile-editor Script" | lolcat
 echo -e "\033[1;33mCreating File Editor!\033[0m"
-echo -e "\033[1;33m"
-chmod +x termux-file-editor
-mv termux-file-editor ~/bin/
+echo -e '\033[1;33m'
+'chmod' +x termux-file-editor
+'mv' termux-file-editor ~/bin/
+echo -e '\033[0m'
 sleep 4
 #Moving Sound Effects
 echo -e "\033[1:34mMoving Sound Effects to Fam-Hacker_Client folder.\033[0m"
-echo -e "\033[1;36m"
-mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
-mv ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
-mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
-mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
-mv ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
-mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
-mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
-echo " "
+echo -e '\033[1;36m'
+'mv' ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
+'mv' ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
+'mv' ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
+'mv' ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
+'mv' ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
+'mv' ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
+'mv' ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
+echo -e '\033[0m'
 echo " "
 sleep 7
 #Cowsay Ending
