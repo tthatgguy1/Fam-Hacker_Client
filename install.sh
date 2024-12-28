a40d82ddd8b629688c03ebbc1fd20fb70f6f6ee8
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2024-12-27 23:35:25 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo -e ".\e(B\e[m"
 sleep 3
 #Install Aria2 Termux
 echo -e "\033[36mInstalling Required Packages\033[0m"
-echo -e "\e[3;1;36m"
+echo -e "\e[1;36m"
 "pkg" install aria2 -y
 ehco -e ".\e(B\e[m" 
 sleep 3
@@ -33,39 +33,38 @@ sleep 5
 #Making Directories
 echo -e "\033[1;34mMaking your new directory to\033[0m \033[1;36mdownload\033[0m \033[1;34mvideos\033[0m"
 echo -e "\e[1;34m" "Creating bin folder\n"
-echo -e "\033[3;1;36m"
+echo -e "\e[1;36m"
 "mkdir" ~/storage/shared/Fam-Hacker_Client
 "mkdir" ~/bin
 "mkdir" ~/storage/shared/sfx
 ehco -e ".\e(B\e[m"
 sleep 3
 #Creating Termux\033[34m-\033[0mFile-Editor Script
-echo -e "\e[3;1;34m"
-echo -e "Creating termux-file-editor script"
-echo -e "\e[1;33mCreating File Editor!\033[0m"
-ehco -e "\e[3;1;36m"
-sleep 3
-"chmod" +x termux-file-editor
-"mv" termux-file-editor ~/bin/
-
+echo -e "\e[1;34m" "Creating termux-file-editor script"
+echo -e "\e[1;33m" "Completing termux-file-editor script"
 ehco -e ".\e(B\e[m"
-sleep 4
+"chmod" +x termux-file-editor
+echo -e "\e[1;33m" " File Editor Created!\n"
+ehco -e "\e[1;36m"
+"mv" termux-file-editor ~/bin/
+sleep 3
+ehco -e ".\e(B\e[m"
 #Moving Sound Effects
-echo -e "\033[1;34mMoving Sound Effects to Fam-Hacker_Client folder.\033[0m"
-echo -e "\033[3;1;36m"
-mv ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
-mv ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
-mv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
-mv ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
-mv ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
-mv ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
-mv ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
+echo -e "\e[1;34m" "Moving Sound Effects to Fam-Hacker_Client folder\n"
+echo -e "\e[1;36m"
+"mv" ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
+"mv" ~/Fam-Hacker_Client/data/High_Value.mp3 ~/storage/shared/High_Value.mp3
+"mv" ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
+"mv" ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
+"mv" ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
+"mv" ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
+"mv" ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
 ehco -e ".\e(B\e[m"
 echo " "
 sleep 7
 #Cowsay Ending
-cowsay -f milk 'Process Complete' | lolcat \-a
-toilet -f big 'Milked' | lolcat \-a
+cowsay -f stimpy 'Process Complete' | lolcat \-a
+toilet -f small 'Milked' | lolcat \-a
 echo " "
 echo " "
 echo " "
