092135dd162f01691f85b9bc9462ef58aad3cf65
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2025-01-05 09:24:14 -0600

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 clear
 'date' | lolcat
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
-sleep 2
+sleep 1
 #Update Packages
 echo " "
 echo -e "\e[5;1;33m" "Updating default packages"
@@ -25,32 +25,44 @@ sleep 2
 echo -e "\e[5;36m" "Installing Required Packages\n"
 echo -e "\e[1;36m"
 "pkg" install aria2 -y
+echo " "
 echo -e ".\e(B\e[m" 
+clear
+echo " "
 sleep 3
 #Request Storage Access
 echo -e "\e[1;34m" "Requesting access to storage!\n"
 echo -e "\e[1;34m" "Allow Storage Permission!\n"
-echo -e "\e[1;36m" "Allow Storage Permission!\n"
+echo -e "\e[1;31m" "Highly recommending that you grant Storage Permission, or you will surely die!\n"
 echo -e "\e[1;36m" 
+echo " "
+sleep 3
 "termux-setup-storage"
 echo -e ".\e(B\e[m"
+echo " "
 sleep 3
 #Making Directories
 echo -e "\033[1;34mMaking your new directory to\033[0m \033[1;36mdownload\033[0m \033[1;34mvideos\033[0m"
 echo -e "\e[1;34m" "Creating bin folder\n"
 echo -e "\e[1;36m"
+echo " "
 "mkdir" ~/storage/shared/Fam-Hacker_Client
 "mkdir" ~/bin
 "mkdir" ~/storage/shared/sfx
 echo -e ".\e(B\e[m"
+echo " "
 sleep 3
 #Creating Termux\033[34m-\033[0mFile-Editor Script
 echo -e "\e[1;34m" "Creating termux-file-editor script\n"
 echo -e "\e[1;33m" "Completing termux-file-editor script\n"
 echo -e ".\e(B\e[m"
+echo " "
+echo " "
 "chmod" +x termux-file-editor
 echo -e "\e[1;33m" " File Editor Created!\n"
 echo -e "\e[1;36m"
+echo " "
+echo " "
 "mv" termux-file-editor ~/bin/
 sleep 3
 echo -e ".\e(B\e[m"
@@ -61,18 +73,20 @@ echo -e "\e[1;36m"
 "mv" ~/Fam-Hacker_Client/data/TLOZ-WW_Heart_Container.mp3 ~/storage/shared/TLOZ-WW_Heart_Container.mp3
 "mv" ~/Fam-Hacker_Client/data/Cooking_Great.mp3 ~/storage/shared/Cooking_Great.mp3
 "mv" ~/storage/shared/sfx ~/storage/shared/Fam-Hacker_Client/
-"mv" ~/storage/shared/High_Value.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/High_Value.mp3
 "mv" ~/storage/shared/TLOZ-WW_Heart_Container.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/TLOZ-WW_Heart_Container.mp3
+"mv" ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
 "mv" ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
 echo -e ".\e(B\e[m"
 echo " "
-sleep 7
+sleep 5
 #Cowsay Ending
-cowsay -f stimpy 'Process Complete' | lolcat \-a
-toilet -f big 'Milked' -F gay
+cowsay -r 'Setting Up' | lolcat \-a
 echo " "
+sleep 3
 echo " "
+toilet -f small 'Fully Installed' -F border -F gay
 echo " "
 echo -e "\033[34mNow you can open any .torrent file and it will open in termux start Downloading.\033[0m"
-echo ""
+echo " "
+echo " "
 sleep 30
