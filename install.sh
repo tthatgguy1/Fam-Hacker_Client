193baf7ac18a8c644a23a195f021029edd0462ec
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2025-08-18 21:37:46 -0500

    Update install.sh
    A lot

--- install.sh
+++ install.sh
@@ -31,69 +31,62 @@ echo " "
 echo " "
 date +"%l:%M:%S %p" | lolcat
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
-sleep 2
+sleep 2.0
 echo " "
 
 ## Update Packages
 echo " "
-echo -e "\033[1;33mUpdating\033[0m \033[1;31m[\033[0m\033[1;33mdefault packages\033[0m\033[1;31m]\033[0m"
+echo -e "\033[1;34mUpdating\033[0m \033[1;36m[\033[0m\033[1;34mdefault packages\033[0m\033[1;36m]\033[0m"
 echo " "
-sleep 2
+sleep 2.0
 echo " "
-echo -e "\033[1;33mStill Updating\033[0m \033[1;31m[\033[0m\033[1;33mdefault packages\033[0m\033[1;31m]\033[0m"
-echo -e "\e[33m"
+echo -e "\033[1;34mStill Updating\033[0m \033[1;36m[\033[0m\033[1;34mdefault packages\033[0m\033[1;36m]\033[0m"
+echo -e "\e[95m"
+echo " "
 "pkg" update -y
 "pkg" upgrade -y
 echo " "
 echo -e " \e(B\e[m" 
-sleep 3
-echo " "
+sleep 3.0
 
 ## Install Aria2 Termux
-echo -e "Installing Required \033[1;31m[\033[0m\033[1;33mPackages\033[0m\033[1;31m]\033[0m"
-echo -e "\e[1;33m"
-"pkg" install aria2 -y
-echo " "
-echo -e " \e(B\e[m" 
+echo -e "\e[34mNow installing\e[0m \e[36maria2c\e[0m"
+"pkg" install aria2 -y 
 echo " "
 echo " "
-sleep 3
+sleep 2.0
 
 ## Request Storage Access
-echo -e "Requesting access to \033[1;31m[\033[0m\033[1;33mstorage!\033[0m\033[1;31m]\033[0m"
-echo -e "\e[1;33m" "Allow Storage Permission!\n"
-echo -e "\e[1;33m" "Highly recommending that you grant Storage Permission, or you will surely die!\n"
-echo -e "\e[31m"
+echo -e "\e[34mYou\e[0m \e[36mwill\e[0m \e[34mneed to give\e[0m \e[36mTermux\e[0m \e[34mstorage\e[0m \e[92mpermissions\e[0m \e[34mto continue\e[0m"
+echo " "
+echo -e "\e[95m"
 "termux-setup-storage"
 echo " "
 echo -e " \e(B\e[m" 
 echo " "
-echo -e "\e[032m" "Making the Directory to download the Vidoes\n"
-sleep 3
-echo " "
+echo -e "\e[34mMaking a new\e[0m \e[36mDirectory\e[0m \e[34mto download\e[0m \e[36mfiles\e[0m \e[34min\e[0m"
+sleep 3.0
 
 ## Making Directories
 echo -e "\033[1;34mMaking your new directory to\033[0m \033[1;36mdownload\033[0m \033[1;34mvideos\033[0m"
-echo -e "\e[1;34m" "Creating bin folder\n"
-echo -e "\e[1;36m"
+echo -e "\e[34mCreating a new\e[0m \e[36mbin\e[0m \e[34mfolder\e[0m"
+echo " "
+echo -e "\e[95m"
 "mkdir" ~/storage/shared/Fam-Hacker_Client
 "mkdir" ~/bin
 "mkdir" ~/storage/shared/sfx
 echo " "
 echo -e " \e(B\e[m" 
 echo " "
-sleep 3
-echo " "
+sleep 3.0
 
 ## Creating Termux\033[34m-\033[0mFile-Editor Script
 echo -e "\033[1;34mCreating\033[0m \033[1;36mtermux\033[0m\033[1;35m-\033[0m\033[1;36mfile\033[0m\033[1;35m-\033[0m\033[1;36meditor\033[0m \033[1;34mscript\033[0m"
-echo -e "\e[1;33m" "Completing termux-file-editor script\n"
-echo -e "\e[1;36m"
+echo -e "\e[34mCompleting\e[0m \e[91mTermux\e[0m\e[93m-\e[0m\e[91mfile\e[0m\e[93m-\e[0m\e[93meditor\e[0m \e[34mscript\e[0m"
+echo -e "\e[95m"
+echo " "
 "chmod" +x termux-file-editor
 echo " "
-echo -e " \e(B\e[m" 
-echo " "
-echo -e "\e[1;36m"
 "mv" termux-file-editor ~/bin/
 echo " "
 echo -e " \e(B\e[m"
@@ -101,8 +94,9 @@ echo " "
 sleep 3
 
 ## Moving Sound Effects
-echo -e "\e[1;34m" "Moving Sound Effects to Fam-Hacker_Client folder\n"
-echo -e "\e[1;36m"
+echo -e "\e[34mMoving\e[0m \e[36mSound Effects\e[0m \e[34mto\e[0m \e[91mFam\e[0m\e[93m-\e[0m\e[91mHacker\e[0m\e[93m_\e[0m\e[91mClient\e[0m \e[34mfolder\e[0m"
+echo " "
+echo -e "\e[95m"
 echo " "
 "mv" ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
 "mv" ~/Fam-Hacker_Client/data/MMF.mp3 ~/storage/shared/MMF.mp3
@@ -116,13 +110,14 @@ echo " "
 echo " "
 echo -e ".\e(B\e[m"
 echo " "
-sleep 3
-echo " "
+sleep 3.0
 clear
 
 ## Cowsay & End Prompt
 cowsay -f meow 'Complete' | lolcat \-a
 toilet -f small 'Install 100%' -F border | lolcat \-a
 echo " "
-echo -e "\e[5;1;33m" "You can open .torrent files with termux when selecting EDIT"
-sleep 30
+echo " "
+echo -e "\e[34mYou can now use\e[0m \e[36mTermux\e[0m \e[34mto download\e[0m \e[93mtorrent\e[0m \e[34mfiles\e[0m"
+cd ..
+read
