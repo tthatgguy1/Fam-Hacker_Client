9291d5b6e4e0c847cbd381766ec2d525cf780f0a
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2025-08-18 23:42:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,6 @@ clear
 echo " "
 echo " "
 date +"%l:%M:%S %p" | lolcat
-mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2.0
 echo " "
 
@@ -38,52 +37,78 @@ echo " "
 echo " "
 echo -e "\033[1;34mUpdating\033[0m \033[1;36m[\033[0m\033[1;34mdefault packages\033[0m\033[1;36m]\033[0m"
 echo " "
-sleep 2.0
 echo " "
-echo -e "\033[1;34mStill Updating\033[0m \033[1;36m[\033[0m\033[1;34mdefault packages\033[0m\033[1;36m]\033[0m"
-echo -e "\e[95m"
+echo -e "\e[34m"
 echo " "
 "pkg" update -y
 "pkg" upgrade -y
 echo " "
-echo -e " \e(B\e[m" 
-sleep 3.0
+echo -e " \e(B\e[m"
+echo " "
+sleep 2.0
+read
+clear
 
 ## Install Aria2 Termux
 echo -e "\e[34mNow installing\e[0m \e[36maria2c\e[0m"
+echo " "
+echo " "
+mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
+echo " "
 "pkg" install aria2 -y 
 echo " "
 echo " "
 sleep 2.0
+read
+clear
 
 ## Request Storage Access
 echo -e "\e[34mYou\e[0m \e[36mwill\e[0m \e[34mneed to give\e[0m \e[36mTermux\e[0m \e[34mstorage\e[0m \e[92mpermissions\e[0m \e[34mto continue\e[0m"
 echo " "
-echo -e "\e[95m"
+echo -e "\e[34m"
+echo " "
 "termux-setup-storage"
 echo " "
 echo -e " \e(B\e[m" 
 echo " "
-echo -e "\e[34mMaking a new\e[0m \e[36mDirectory\e[0m \e[34mto download\e[0m \e[36mfiles\e[0m \e[34min\e[0m"
+echo " "
 sleep 3.0
+read
+clear
 
 ## Making Directories
+echo " "
 echo -e "\033[1;34mMaking your new directory to\033[0m \033[1;36mdownload\033[0m \033[1;34mvideos\033[0m"
+echo " "
+echo " "
+mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
+sleep 2.0
+echo " "
 echo -e "\e[34mCreating a new\e[0m \e[36mbin\e[0m \e[34mfolder\e[0m"
 echo " "
-echo -e "\e[95m"
+echo " "
+mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
+sleep 2.0
+echo " "
+echo -e "\e[34m"
 "mkdir" ~/storage/shared/Fam-Hacker_Client
 "mkdir" ~/bin
 "mkdir" ~/storage/shared/sfx
 echo " "
 echo -e " \e(B\e[m" 
 echo " "
-sleep 3.0
+read
+clear
 
 ## Creating Termux\033[34m-\033[0mFile-Editor Script
+echo " "
 echo -e "\033[1;34mCreating\033[0m \033[1;36mtermux\033[0m\033[1;35m-\033[0m\033[1;36mfile\033[0m\033[1;35m-\033[0m\033[1;36meditor\033[0m \033[1;34mscript\033[0m"
+echo " "
+echo " "
+mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
+echo " "
 echo -e "\e[34mCompleting\e[0m \e[91mTermux\e[0m\e[93m-\e[0m\e[91mfile\e[0m\e[93m-\e[0m\e[93meditor\e[0m \e[34mscript\e[0m"
-echo -e "\e[95m"
+echo -e "\e[34m"
 echo " "
 "chmod" +x termux-file-editor
 echo " "
@@ -96,7 +121,7 @@ sleep 3
 ## Moving Sound Effects
 echo -e "\e[34mMoving\e[0m \e[36mSound Effects\e[0m \e[34mto\e[0m \e[91mFam\e[0m\e[93m-\e[0m\e[91mHacker\e[0m\e[93m_\e[0m\e[91mClient\e[0m \e[34mfolder\e[0m"
 echo " "
-echo -e "\e[95m"
+echo -e "\e[34m"
 echo " "
 "mv" ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
 "mv" ~/Fam-Hacker_Client/data/MMF.mp3 ~/storage/shared/MMF.mp3
@@ -110,7 +135,7 @@ echo " "
 echo " "
 echo -e ".\e(B\e[m"
 echo " "
-sleep 3.0
+read
 clear
 
 ## Cowsay & End Prompt
