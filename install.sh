0e4371b9b2457d4a2ce7b9750e934426dd41cc68
Seth J. Nelson <68300078+tthatgguy1@users.noreply.github.com>
2025-08-19 08:57:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@
 clear
 mpv ~/Fam-Hacker_Client/data/MMF.mp3 >/dev/null 2>/dev/null
 echo " "
-echo " "
 echo '
 ──███████╗╔█████╗─███╗───███╗─██╗──██╗╔█████╗─╔██████╗██╗──██╗██████╗─
 ──██╔════╝██╔══██╗████╗─████║─██║──██║██╔══██╗██╔════╝██║─██╔╝██╔══██╗
@@ -28,10 +27,7 @@ clear
 
 ## Logo 2
 echo " "
-echo " "
-date +"%l:%M:%S %p" | lolcat
-sleep 2.0
-echo " "
+date +"%l:%M:%S %p" | lolcat --animate -d 20
 echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
@@ -41,19 +37,12 @@ clear
 echo " "
 date +"%l:%M:%S %p" | lolcat
 echo " "
-echo " "
 echo -e "\033[1;34mUpdating\033[0m \033[1;36m[\033[0m\033[1;34mdefault packages\033[0m\033[1;36m]\033[0m"
-echo " "
-echo " "
 echo -e "\e[34m"
-echo " "
 "pkg" update -y
 "pkg" upgrade -y
-echo " "
 echo -e " \e(B\e[m"
-echo " "
 sleep 2.0
-echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
 clear
@@ -62,17 +51,12 @@ clear
 echo " "
 date +"%l:%M:%S %p" | lolcat
 echo " "
-echo " "
 echo -e "\e[34mNow installing\e[0m \e[36maria2c\e[0m"
-echo " "
-echo " "
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
-echo " "
-"pkg" install aria2 -y 
-echo " "
-echo " "
+echo -e "\e[34m"
+"pkg" install aria2 -y
+echo -e " \e(B\e[m"
 sleep 2.0
-echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
 clear
@@ -81,41 +65,31 @@ clear
 echo " "
 date +"%l:%M:%S %p" | lolcat
 echo " "
-echo " "
 echo -e "\e[34mYou\e[0m \e[36mwill\e[0m \e[34mneed to give\e[0m \e[36mTermux\e[0m \e[34mstorage\e[0m \e[92mpermissions\e[0m \e[34mto continue\e[0m"
-echo " "
 echo -e "\e[34m"
-echo " "
 "termux-setup-storage"
-echo " "
 echo -e " \e(B\e[m" 
-echo " "
-echo " "
 sleep 2.0
-echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
 clear
 
-## Making Directories
+## Pre-Directory
 echo " "
 date +"%l:%M:%S %p" | lolcat
 echo " "
-echo " "
 echo -e "\033[1;34mMaking your new directory to\033[0m \033[1;36mdownload\033[0m \033[1;34mvideos\033[0m"
-echo " "
-echo " "
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2.0
-echo " "
-echo " "
 echo -e "\e[34mCreating necessary\e[0m \e[36mdirectories\e[0m \e[34mfor this script\e[0m\e[36m.\e[0m"
-echo " "
-echo " "
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
 sleep 2.0
+
+## Making Directories
 echo " "
 echo " "
+"mkdir" ~/storage/shared/NFS-Flud-Pit
+echo -e "\e[32mCreated\e[0m \e[36mNFS\e[0m\e[35m-\e[0m\e[36mFlud\e[0m\e[35m-\e[0m\e[36mPit\e[0m"
 "mkdir" ~/storage/shared/Fam-Hacker_Client
 echo -e "\e[32mCreated\e[0m \e[36mFam\e[0m\e[35m-\e[0m\e[36mHacker\e[0m\e[35m_\e[0m\e[36mClient\e[0m"
 sleep 2.0
@@ -127,11 +101,8 @@ echo -e "\e[32mCreated\e[0m \e[36msfx\e[0m"
 sleep 2.0
 "mkdir" ~/storage/shared/NFS-Flud-Pit
 echo -e "\e[32mCreated\e[0m \e[36mNFS\e[0m\e[35m-\e[0m\e[36mFlud\e[0m\e[35m-\e[0m\e[36mPit\e[0m"
-echo " "
-echo " "
-echo -e " \e(B\e[m" 
-echo " "
 sleep 2.0
+echo " "
 echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
@@ -141,38 +112,22 @@ clear
 echo " "
 date +"%l:%M:%S %p" | lolcat
 echo " "
-echo " "
-echo " "
 echo -e "\033[1;34mCreating\033[0m \033[1;36mtermux\033[0m\033[1;35m-\033[0m\033[1;36mfile\033[0m\033[1;35m-\033[0m\033[1;36meditor\033[0m \033[1;34mscript\033[0m"
-echo " "
-echo " "
-echo " "
-echo " "
-sleep 3.0
-echo -e "\033[1;34mCompleting\033[0m \033[1;36mtermux\033[0m\033[1;35m-\033[0m\033[1;36mfile\033[0m\033[1;35m-\033[0m\033[1;36meditor\033[0m \033[1;34mfile\033[0m"
 mpv ~/Fam-Hacker_Client/data/Cooking_Great.mp3 >/dev/null 2>/dev/null
+sleep 3.0
 echo " "
-sleep 2.0
 echo " "
-echo -e "\e[34m"
-echo " "
+echo -e "\033[1;34mCompleting\033[0m \033[1;36mtermux\033[0m\033[1;35m-\033[0m\033[1;36mfile\033[0m\033[1;35m-\033[0m\033[1;36meditor\033[0m \033[1;34mfile\033[0m"
+sleep 3.0
 "chmod" +x termux-file-editor
-echo " "
 "mv" termux-file-editor ~/bin/
-echo " "
-echo -e " \e(B\e[m"
-echo " "
-sleep 2.0
-echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
 clear
 
 ## Moving Sound Effects
 echo -e "\e[34mMoving\e[0m \e[36mSound Effects\e[0m \e[34mto\e[0m \033[1;36mFam\033[0m\033[1;35m-\033[0m\033[1;36mHacker\033[0m\033[1;35m_\033[0m\033[1;36mClient\033[0m"
-echo " "
 echo -e "\e[34m"
-echo " "
 "mv" ~/Fam-Hacker_Client/data/Secret_Sound.mp3 ~/storage/shared/Secret_Sound.mp3
 "mv" ~/Fam-Hacker_Client/data/MMF.mp3 ~/storage/shared/MMF.mp3
 "mv" ~/Fam-Hacker_Client/data/TLOZ-WW_Heart_Container.mp3 ~/storage/shared/TLOZ-WW_Heart_Container.mp3
@@ -183,19 +138,16 @@ echo " "
 "mv" ~/storage/shared/Cooking_Great.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Cooking_Great.mp3
 "mv" ~/storage/shared/Secret_Sound.mp3 ~/storage/shared/Fam-Hacker_Client/sfx/Secret_Sound.mp3
 echo " "
-echo -e ".\e(B\e[m"
-echo " "
-sleep 2.0
-echo " "
 echo -e "\e[35m[\e[0m\e[36mPress\e[0m\e[35m]\e[0m \e[34mPress\e[0m \e[36many\e[0m \e[34mbutton to continue\e[0m"
 read
 clear
 
 ## Cowsay & End Prompt
+echo " "
+date +"%l:%M:%S %p" | lolcat --animate -d 10
+echo " "
 cowsay -f meow 'Complete' | lolcat \-a
 toilet -f mono9 'Complete' -F border | lolcat \-a
-echo " "
-echo " "
 echo " "
 echo -e "\e[34mYou can now use\e[0m \e[36mTermux\e[0m \e[34mto download\e[0m \e[93mtorrent\e[0m \e[34mfiles\e[0m"
 cd ..
